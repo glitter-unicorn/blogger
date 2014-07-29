c04cf749469b19f905547989cc9f352917cb3444
Laurel Nason <laurel_anne89@yahoo.com>
2014-07-29 11:03:21 -0400

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,7 @@ class ArticlesController < ApplicationController
   end
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 end
-
