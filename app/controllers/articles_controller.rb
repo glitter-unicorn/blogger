e0f02aa8e9a219735f614907140744d48f410312
Laurel Nason <laurel_anne89@yahoo.com>
2014-07-29 13:40:14 -0400

    Added comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,50 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
     @comment = Comment.new
     @comment.article_id = @article.id
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+  def destroy
+    @article = Article.destroy(params[:id])
+
+    redirect_to article_index(@article)
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article" '#{@article.title}'
+
+    redirect_to article_path(@article)
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
+  def destroy
+   @article = Article.find(params[:id])
+ end
 end
