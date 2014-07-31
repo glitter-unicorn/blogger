4d242d58612ed79510d55a833a52d89f8c106bee
Laurel Nason <laurel_anne89@yahoo.com>
2014-07-31 12:23:07 -0400

    added a few gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,15 +1,35 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id                 :integer          not null, primary key
+#  title              :string(255)
+#  body               :text
+#  created_at         :datetime
+#  updated_at         :datetime
+#  image_file_name    :string(255)
+#  image_content_type :string(255)
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#
+
 class Article < ActiveRecord::Base
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+
+  has_attached_file :image
+  validates_attachment_content_type :image, 
+                                    :content_type => ["image/jpg", "image/jpeg", "image/png"]
   def tag_list
     self.tags.collect do |tag|
       tag.name
     end.join(", ")
-    def tag_list=(tags_string)
-     tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
-     new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
-     self.tags = new_or_found_tags
-   end
- end
+  end
+  
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 end
